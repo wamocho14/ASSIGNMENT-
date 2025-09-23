ca3cc2b03044803dfe0e987443b484310c1b80de
wamocho14 <ingriff7@gmail.com>
2025-09-23 12:36:29 +0300

    variable height.c
    a c programme to display height

---
+++ program file.c
@@ -0,0 +1,17 @@
+// hello world
+/*
+NAME: GRIFFIN MUTALI
+REG NO: PA106/G/28744/25
+DESCRIPTION: PROGRAMME TO PROMPT AGE
+*/
+#include<stdio.h>preprpcessor directive scanf(),
+
+//main function
+int main()
+{
+	int height;
+	printf("enter your height in foots: ");
+	scanf("%d",&height);
+	printf("your height is: %d Feet\n",height);
+	return 0;
+}
