9682b38937787d3c8d7604900d4c7c9c913e7ac6
wamocho14 <ingriff7@gmail.com>
2025-09-23 12:59:03 +0300

    Add files via upload
    PROGRAMME TO DISPLAY THE ID NUMBER

---
+++ VARIABLE ID NUMBER.c
@@ -0,0 +1,17 @@
+// hello world
+/*
+NAME: GRIFFIN MUTALI
+REG NO: PA106/G/28744/25
+DESCRIPTION: PROGRAMME TO PROMPT ID NUUMBER
+*/
+#include<stdio.h> preprpcessor directive scanf(),
+
+//main function
+int main()
+{
+	int ID_NUMBER;
+	printf("enter your ID NUMBER: ");
+	scanf("%d",&ID_NUMBER);
+	printf("your ID NUMBER is: %d\n",ID_NUMBER);
+	return 0;
+}
