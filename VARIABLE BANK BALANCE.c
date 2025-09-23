a95a4e8416d4ab1c6e11907af66f0e14cc4a219e
wamocho14 <ingriff7@gmail.com>
2025-09-23 13:08:21 +0300

    Add files via upload
    PROGRAMME TO DISPLAY BANK BALANCE

---
+++ VARIABLE BANK BALANCE.c
@@ -0,0 +1,17 @@
+// hello world
+/*
+NAME: GRIFFIN MUTALI
+REG NO: PA106/G/28744/25
+DESCRIPTION: PROGRAMME TO PROMPT BANK BALANCE
+*/
+#include<stdio.h>preprpcessor directive scanf(),
+
+//main function
+int main()
+{
+	int BANK_BALANCE;
+	printf("enter your BANK BALANCE in Ksh.: ");
+	scanf("%d",&BANK_BALANCE);
+	printf("your BANK BALNCE is: %d Ksh\n",BANK_BALANCE);
+	return 0;
+}
