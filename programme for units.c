ac0912b40d24894d6c72428bacc4d86a1a79865e
wamocho14 <ingriff7@gmail.com>
2025-09-24 12:04:55 +0300

    Add files via upload
    programme  to display units

---
+++ programme for units.c
@@ -0,0 +1,19 @@
+//HELLO WORLD
+/*
+NAME: GRIFFIN MUTALI
+REG NO: PA106/G/28744/25
+DESCRIPTION:PROGRAMME TO DISPLAY ALL UNITS OF THIS SEMISTER
+*/
+#include <stdint.h>preprcessor directive:scanf(),
+//main function
+int main()
+{
+	printf("Descrete mathematics \n");
+	printf("Mathematics for science \n");
+	printf("Fundamentals of ICT\n");
+	printf("Computer Systems ans Organisation\n");
+	printf("Communication Skills\n");
+	printf("Introduction to Programming & Problem Solving\n");
+	printf("Fundamentals of System Analysis & Design\n");
+	return 0;
+}
