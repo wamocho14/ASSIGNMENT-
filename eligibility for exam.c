8e751b8400e5a1e6a8fb3e508d9af70938a5262b
wamocho14 <ingriff7@gmail.com>
2025-10-01 22:41:01 +0300

    Add files via upload
    program for eligibilty for final exam

---
+++ eligibility for exam.c
@@ -0,0 +1,26 @@
+//HELLO WORLD
+/*
+NAME:GRIFFIN MUTALI
+REG NO:PA106/G/28744/25
+DATE:30/09/2025
+DESCRIPTION:A PROGRAM TO CHECK FOR ELIGIBILITY FOR FINAL EXAMS
+*/
+
+#include <stdio.h>
+int main()
+{
+	int attendance,marks;
+	printf("\nPLAESE ENTER YOUR ATTENDANCE: ");
+	scanf("%d",&attendance !=1);
+	printf("\nPLEASE ENTER YOUR AVERAGE MARKS ");
+	scanf("%d",&marks);
+	if(attendance>=75&&marks>=40){
+		printf("YOU ARE  ELIGIBLE FOR THE EXAM");
+	}
+	
+		else
+	    {	printf("YOU NOT ELIGIBLE FOR THE LOAN");
+	}
+	
+	return 0;
+}
