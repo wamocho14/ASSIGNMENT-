d29f8713df7033e5a24ef1ecbff8fd8753df76bf
wamocho14 <ingriff7@gmail.com>
2025-10-23 23:26:21 +0300

    electricbill.c

--- electricbill.c
+++ electricbill.c
@@ -1,28 +1,37 @@
-//Hello World
+//HELLO WOLRLD
 /*
 NAME: GRIFFIN MUTALI
-REG NO: PA06/G/28744/25
+REG NO: PA106/G/28744
 DESCRIPTION: PROGRAMME TO CALCULATE ELECTRIC BILL
 */
+ 
+ #include<stdio.h>
+ 	double calculateElecricBill(int units){
+	double bill=0.0; 
+ 	if (units<=100)
+	 {
+	 	bill=units*10;
+	 	
+     }
+	 
+	  else if (units<=200)
+	 {
+	 	bill=(100*10)+(units-100)*15;
 
-#include <stdio.h>
-int main()
-{
-	int unit,bill;
-	printf("\nfirst 100 units = Ksh 10 per unit");
-	printf("\nnext 100 units = Ksh. 15 per unit");
-	printf("\nabove 200 units = Kh. 20 per unit");
-	printf();
-	if (unit<=100){
-	bill=unit*10;
-	}
-	else if(unit>=100<200){
-		bill=unit*15;
-	}
-	else {
-		bill=unit*20;
-	}
-	
-		
-	return 0;
-}
+	 }
+	 else
+	 {
+	 	bill=(100*10)+(100*15)+(units-200)*20;	
+	 }
+	 	return bill;
+     }
+     int main()
+	 {
+		int units;
+		 	printf("Enter the number of units consumed: ");
+ 	scanf("%d", &units);
+ 	double total=calculateElecricBill(units);
+ 	printf("Total bill= Ksh.%.2f\n",total);
+		 return 0;
+	 }
+ 	
